3491a59eb997e64c638d29ab28bd11edef091f1f
maxwellre <yitianshao@gmail.com>
2018-03-06 00:01:02 -0800

    Prepare program for hit wall event simulation

---
+++ DisplayEventData.m
@@ -0,0 +1,18 @@
+%% Display cursor position
+close all
+clear all
+% -------------------------------------------------------------------------
+% Import data
+PosiFileName = 'EventData.csv';
+eventData = csvread(PosiFileName);
+
+Fs = 1000;
+Ts = 1/Fs;
+
+% -------------------------------------------------------------------------
+% Display cursor axis trajectory
+axisLabel = {'X','Y','Z'};
+figure('Position',[80,80,800,600])
+plot(eventData(:,1)*Ts,eventData(:,2));
+ylabel('Amplitude');
+xlabel('Time (secs)');
